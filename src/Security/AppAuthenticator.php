0f3dca60566f1dfb4a45b6e2b1c6cefefa027f3d
Saveleva Margarita <margarita.saveleva1@gmail.com>
2021-10-05 09:39:52 +0200

    route reconfigured pour after login redirecting

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -95,7 +95,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse("/");
+        return new RedirectResponse($this->urlGenerator->generate("event"));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
