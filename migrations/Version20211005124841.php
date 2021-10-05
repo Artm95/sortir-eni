4db11a49776903164ad84d6037c64eeeaea8196b
dies-iris <51315734+dies-iris@users.noreply.github.com>
2021-10-05 16:15:37 +0200

    chore: upd migration

--- migrations/Version20211005124841.php
+++ migrations/Version20211005124841.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211005123056 extends AbstractMigration
+final class Version20211005124841 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20211005123056 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE participant CHANGE user_name nickname VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE participant ADD nickname VARCHAR(255) DEFAULT NULL, CHANGE user_name photo VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE participant CHANGE nickname user_name VARCHAR(255) CHARACTER SET utf8mb3 DEFAULT NULL COLLATE `utf8mb3_unicode_ci`');
+        $this->addSql('ALTER TABLE participant ADD user_name VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_unicode_ci`, DROP photo, DROP nickname');
     }
 }
