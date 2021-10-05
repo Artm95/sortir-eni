b08af77815ee3ae768c4ce19a17f264ffd6c6d59
Arthur Mallet <malleta01@gmail.com>
2021-10-05 08:55:17 +0200

    most of the homepage's front done; work on the search query started

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\SearchEventType;
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class EventController extends AbstractController
-{
-    #[Route('/event', name: 'event')]
-    public function index(): Response
-    {
+class EventController extends AbstractController {
+    #[Route('/', name: 'event')]
+    public function index(Request $request, EventRepository $repository): Response {
+        $form = $this->createForm(SearchEventType::class, [
+            'action' => $this->generateUrl('event'),
+            'method' => 'GET',
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $events = $repository->getAllWithOrganizer();
+        } else {
+            $events = $repository->getAllWithOrganizer();
+        }
+
         return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
+            'events' => $events,
+            'searchForm' => $form->createView()
+        ]);
+    }
+
+    #[Route(
+        path: '/sortie/{id}',
+        name: 'event_detail',
+        requirements: ['id' => '\d+']
+    )]
+    public function detail(int $id, EventRepository $repository): Response {
+        $event = $repository->find($id);
+
+        return $this->render('event/detail.html.twig', [
+            'event' => $event,
         ]);
     }
 }
