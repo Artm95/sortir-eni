e1ba6304209bf8aa6b4f79216755a862dc3ee6c7
Saveleva Margarita <margarita.saveleva1@gmail.com>
2021-10-06 10:12:59 +0200

    show user profil page finished + add avatar function done

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -6,17 +6,19 @@ use App\Entity\Campus;
 use App\Entity\Participant;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
 class ProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('userName', TextType::class, [
+            ->add('nickname', TextType::class, [
                 // 'label_attr' => [
                 //     'class' => '`checkbox-inline'
                 // ],
@@ -67,7 +69,14 @@ class ProfileType extends AbstractType
                 'choice_label' => 'name',
                 'label' => 'Campus'
                 // 'choice_value' => 'id'
-            ]);;
+            ])
+            ->add('avatar', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new Image()
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
